82744f757f2605bf7c65c85cdfd1d8a85fe10d55
Luis Eduardo Hoshina <duhoshina@gmail.com>
2024-02-07 16:50:31 -0300

    feat: separei cada responsabilidade por função

--- app/api/calcular/route.ts
+++ app/api/calcular/route.ts
@@ -1,67 +1,71 @@
 const settings: SettingsSchema = {
   game: {
     quantidade_maxima_partidas: 10,
-    quantidade_jogadores: 10,
-    chance_ganhar_jogo: 0.48,
+    quantidade_jogadores: 1500,
+    chance_ganhar_jogo: 0.486,
   },
   players: {
-    saldo_inicial: 200.0,
-    valor_cada_aposta: 22.0,
+    saldo_inicial: 1000.0,
+    valor_cada_aposta: 100.0,
   }
 };
 
 export function GET(req: Request){
   let jogadores: Jogador[] = [];
+  let lucro_cassino = 0;
 
-  const partida = gerarPartida(jogadores, settings)
-  
-  return Response.json(partida);
-}
-
-function gerarPartida(jogadores: Jogador[], settings: SettingsSchema) {
-
-  for (let i = 0; i < settings.game.quantidade_jogadores; i++) {
+  for(let i = 0; i < settings.game.quantidade_jogadores; i++){
     jogadores.push({
       index: i,
       saldo: settings.players.saldo_inicial,
     });
   }
 
-  let quantidade_pessoas_lucro = 0;
-  let quantidade_pessoas_prejuizo = 0;
-  let total_apostado = settings.players.valor_cada_aposta * jogadores.length;
-  let lucro_cassino = 0;
+  let quantidade_partidas = 0;
+  
+  for(let i = 0; i < settings.game.quantidade_maxima_partidas; i++){
+    lucro_cassino = gerarPartida(jogadores, lucro_cassino, settings);
+    quantidade_partidas++
+  }
 
+  let pessoas_lucro = 0;
+  let pessoas_prejuizo = 0;
+
+  for(const jogador of jogadores){
+    if(jogador.saldo > settings.players.saldo_inicial){
+      pessoas_lucro++
+    } else {
+      pessoas_prejuizo++
+    }
+  }
+
+  const res = {
+    status: 200,
+    settings,
+    data: {
+      quantidade_partidas,
+      lucro_cassino,
+      pessoas_lucro,
+      pessoas_prejuizo,
+    }
+  }
+  
+  return Response.json(res);
+}
+
+function gerarPartida(jogadores: Jogador[], lucro_cassino: number, settings: SettingsSchema) {
   for(let jogador of jogadores){
     const resultadoAposta = Math.random() < settings.game.chance_ganhar_jogo;
 
     if (resultadoAposta){
       jogador.saldo += settings.players.valor_cada_aposta * 2;
-      quantidade_pessoas_lucro++;
     } else {
       jogador.saldo -= settings.players.valor_cada_aposta;
       lucro_cassino += settings.players.valor_cada_aposta;
-      quantidade_pessoas_prejuizo++
     }
-
-    console.log(`jogador[${jogador.index}]: R$${jogador.saldo}`);
   }
 
-  console.log(`total apostado: R$${total_apostado}`);
-  console.log(`lucro cassino: R$${lucro_cassino}`);
-  console.log(`pessoas no lucro: ${quantidade_pessoas_lucro}`);
-  console.log(`pessoas no prejuizo: ${quantidade_pessoas_prejuizo}`);
-
-  return {
-    status: 200,
-    settings,
-    data: {
-      quantidade_pessoas_lucro,
-      quantidade_pessoas_prejuizo,
-      total_apostado,
-      lucro_cassino,
-    },
-  };
+  return lucro_cassino;
 }
 
 interface SettingsSchema {
